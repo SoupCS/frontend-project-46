e4f996085c0a3cfcba3db995566dd2dac2a4d6f3
YoungSmoke <starodubtsev.07@mail.ru>
2026-01-29 15:49:07 +0700

    test

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,33 +1,33 @@
-import _ from 'lodash';
+import _ from 'lodash'
 
 const formattingValue = (value) => {
   if (_.isObjectLike(value)) {
-    return '[complex value]';
+    return '[complex value]'
   }
-  return _.isString(value) ? `'${value}'` : value;
-};
+  return _.isString(value) ? `'${value}'` : value
+}
 
 const formatToPlain = (data) => {
   const getResult = (tree, path) => tree.flatMap((node) => {
-    const newPath = [...path, node.key];
+    const newPath = [...path, node.key]
 
     switch (node.type) {
       case 'nested':
-        return getResult(node.children, newPath);
+        return getResult(node.children, newPath)
       case 'added':
-        return `Property '${newPath.join('.')}' was added with value: ${formattingValue(node.value)}`;
+        return `Property '${newPath.join('.')}' was added with value: ${formattingValue(node.value)}`
       case 'deleted':
-        return `Property '${newPath.join('.')}' was removed`;
+        return `Property '${newPath.join('.')}' was removed`
       case 'changed':
-        return `Property '${newPath.join('.')}' was updated. From ${formattingValue(node.oldValue)} to ${formattingValue(node.newValue)}`;
+        return `Property '${newPath.join('.')}' was updated. From ${formattingValue(node.oldValue)} to ${formattingValue(node.newValue)}`
       case 'unchanged':
-        return [];
+        return []
       default:
-        throw new Error(`Wrong node type: ${node.type}!`);
+        throw new Error(`Wrong node type: ${node.type}!`)
     }
-  });
-  const result = getResult(data, []);
-  return result.join('\n');
-};
+  })
+  const result = getResult(data, [])
+  return result.join('\n')
+}
 
-export default formatToPlain;
+export default formatToPlain
